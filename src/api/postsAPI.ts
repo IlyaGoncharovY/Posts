90acd456fc4b94635250fa9c0067a67408fb59fc
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-05-25 19:35:54 +0300

    feat (postsAPI) added Posts.tsx

---
+++ src/api/postsAPI.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com/',
+    // withCredentials: true
+})
+
+export const postsAPI = {
+    getPosts() {
+        return instance.get<ResponseGetPostType>("posts")
+    },
+    getUser() {
+        return instance.get<ResponseGetUsersType>("users")
+    }
+}
+
+export type ResponseGetPostType = {
+    userId: number
+    id: number
+    title: string
+    body: string
+}
+
+export type ResponseGetUsersType = {
+	id: number
+	name: string
+	username: string
+	email: string
+	address: ResponseGetUsersTypeAddress
+	phone: string
+	website: string
+	company: ResponseGetUsersTypeCompany
+}
+export type ResponseGetUsersTypeAddressGeo = {
+	lat: string
+	lng: string
+}
+export type ResponseGetUsersTypeAddress = {
+	street: string
+	suite: string
+	city: string
+	zipcode: string
+	geo: ResponseGetUsersTypeAddressGeo;
+}
+export type ResponseGetUsersTypeCompany = {
+	name: string
+	catchPhrase: string
+	bs: string
+}
