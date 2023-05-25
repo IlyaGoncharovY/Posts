bf99671c0e46be2a00965f334cd839c1605dad47
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-05-26 00:07:55 +0300

    feat (App) added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Navigate, Route, Routes} from 'react-router-dom';
 import './App.css';
 import {Posts} from "./components/postsList/Posts";
+import {PATH} from "./utils/path";
+import {AboutMe} from "./components/aboutMe/AboutMe";
+import {InfoUser} from "./components/userDetails/InfoUser";
 
 function App() {
     return (
         <div className="App">
-            <Posts/>
+            <Routes>
+                <Route path={PATH.POST_PAGE} element={<Posts/>}/>
+                <Route path={PATH.ABOUT_ME} element={<AboutMe/>}/>
+                <Route path={PATH.INFO_USER} element={<InfoUser/>}/>
+                <Route path={PATH.MAIN} element={<Navigate to={PATH.POST_PAGE}/>}/>
+            </Routes>
         </div>
     );
 }
---
+++ src/utils/path.ts
@@ -0,0 +1,6 @@
+export const PATH = {
+    POST_PAGE: "/postPage",
+    ABOUT_ME: "/aboutMe",
+    INFO_USER: "/infoUser",
+    MAIN: "*"
+}
