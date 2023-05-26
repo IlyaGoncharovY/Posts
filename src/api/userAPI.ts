903f3f1614919a6c3638c05a85a6e360992a2ff2
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-05-26 23:20:12 +0300

    feat (userAPI) added userAPI.ts

---
+++ src/api/userAPI.ts
@@ -0,0 +1,34 @@
+import {instance} from "./postsAPI";
+
+export const userAPI = {
+    getUsers(userId: number) {
+        return instance.get<ResponseUserType>(`users?id=${userId}`)
+    }
+}
+
+export type ResponseUserType = {
+	id: number;
+	name: string;
+	username: string;
+	email: string;
+	address: ResponseUserTypeAddress;
+	phone: string;
+	website: string;
+	company: ResponseUserTypeCompany;
+}
+export type ResponseUserTypeAddressGeo = {
+	lat: string;
+	lng: string;
+}
+export type ResponseUserTypeAddress = {
+	street: string;
+	suite: string;
+	city: string;
+	zipcode: string;
+	geo: ResponseUserTypeAddressGeo;
+}
+export type ResponseUserTypeCompany = {
+	name: string;
+	catchPhrase: string;
+	bs: string;
+}
