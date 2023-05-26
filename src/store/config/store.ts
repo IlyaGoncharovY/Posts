cf37b9148c810a6053c7eca8042af96768dc3123
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-05-27 00:23:53 +0300

    fix (store) added userSlice.ts

--- src/store/config/store.ts
+++ src/store/config/store.ts
@@ -3,19 +3,22 @@ import {takeEvery} from "redux-saga/effects"
 import createSagaMiddleware from "redux-saga";
 import posts, {GET_POSTS, getPostsSagaWorker} from "../slices/postSlice";
 import comments, {GET_COMMENTS, getCommentsSagaWorker} from "../slices/commentSlice";
+import user, {GET_USERS, getUsersSagaWorker} from "../slices/userSlice";
 
 const sagaMiddleware = createSagaMiddleware()
 
 function* sagasWatcher() {
     yield takeEvery(GET_POSTS, getPostsSagaWorker)
     yield takeEvery(GET_COMMENTS, getCommentsSagaWorker)
+    yield takeEvery(GET_USERS, getUsersSagaWorker)
 }
 
 
 export const store = configureStore({
     reducer: {
         posts,
-        comments
+        comments,
+        user
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({thunk: false}).concat(sagaMiddleware)
