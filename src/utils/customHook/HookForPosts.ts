42c63b4b718748a78e59ca05405e722ad9414026
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-05-28 00:17:39 +0300

    feat (custom hook) added custom hook. logic post component

---
+++ src/utils/customHook/HookForPosts.ts
@@ -0,0 +1,45 @@
+import {useAppDispatch, useAppSelector} from "../../store/config/hook";
+import React, {useState} from "react";
+import {getPostsAC, nextPage, prevPage} from "../../store/slices/postSlice";
+
+export const usePostsForRender = () => {
+
+    // const posts = useAppSelector(state => state.posts.posts)
+    const filteredPosts = useAppSelector(state => state.posts.filteredPosts)
+    const postsPage = useAppSelector(state => state.posts.postsPage)
+
+    //limit page number
+    const maxPage = 10
+    const minPage = 1
+
+    const [searchValue, setSearchValue] = useState<string>("");
+
+    const filteredPostsByTitle = searchValue
+        ? filteredPosts.filter(post => post.title.includes(searchValue))
+        : filteredPosts;
+
+    const dispatch = useAppDispatch()
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value);
+    };
+
+    const handleClear = () => {
+        setSearchValue('');
+        dispatch(getPostsAC(postsPage));
+    };
+
+    const nextPageHandler = () => dispatch(nextPage(postsPage));
+    const prevPageHandler = () => dispatch(prevPage(postsPage));
+
+    return {
+        filteredPostsByTitle,
+        postsPage,
+        searchValue,
+        handleSearch,
+        handleClear,
+        nextPageHandler,
+        prevPageHandler,
+        minPage, maxPage
+    }
+}
